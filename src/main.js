d0284a5f99c87dc8aa0fc6d0707c9d1421d45434
Zara-Toorox <sulfat-klingeln.7v@icloud.com>
2025-12-30 19:28:18 +0100

    CSS/JS Pfade korrigiert für GitHub Pages
    - CSS direkt in HTML eingebunden statt über JS imports
    - Relative Pfade statt absolute Pfade
    - main.js vereinfacht (CSS imports entfernt)

--- src/main.js
+++ src/main.js
@@ -3,20 +3,6 @@
  * Solar Forecast ML Website
  */
 
-// Import styles
-import './styles/variables.css'
-import './styles/reset.css'
-import './styles/layout.css'
-import './styles/components.css'
-
-// Import page-specific styles based on current page
-const currentPath = window.location.pathname;
-if (currentPath.includes('wissensbasis')) {
-    import('./styles/wissensbasis.css');
-} else {
-    import('./styles/home.css');
-}
-
 // Import modules
 import { initTheme } from './js/theme.js'
 import { initNavigation, initSmoothScroll } from './js/navigation.js'
